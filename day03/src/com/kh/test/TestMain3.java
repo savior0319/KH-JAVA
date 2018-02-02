d874037579043059f3cf87e5b102e8b115e057ab
my Name <me@example.com>
2018-02-02 17:53:36 +0900

    -

--- day03/src/com/kh/test/TestMain3.java
+++ day03/src/com/kh/test/TestMain3.java
@@ -1,16 +1,19 @@
 package com.kh.test;
 
+import java.util.Scanner;
+
 public class TestMain3 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("소문자 입력 : ");
+		char ch = sc.next().charAt(0);
 
-		int a = 10;
-		int b = 20;
+		System.out.println();
+		System.out.println("---대문자1---");
+		String str = String.valueOf(ch).toUpperCase();
+		System.out.println(str);
+		System.out.println("---대문자2---");
 
-		System.out.println(a > b);
-		System.out.println(a < b);
-		System.out.println(a == b);
-		System.out.println(a != b);
-		System.out.println(a <= b);
-		System.out.println(a >= b);
+		System.out.println((char) (ch ^ 32));
 	}
 }
