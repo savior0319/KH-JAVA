39f5c159c7055e25abe298e38232fadf255a2fdc
my Name <me@example.com>
2018-02-05 10:48:58 +0900

    -

--- day04/src/kh/java/test/testMain.java
+++ day04/src/kh/java/test/testMain.java
@@ -1,9 +1,20 @@
 package kh.java.test;
 
+import java.util.Scanner;
+
 public class testMain {
 
 	public static void main(String[] args) {
-	
+		Scanner sc = new Scanner(System.in);
+
+		int num;
+
+		System.out.print("10을 입력하세요 : ");
+		num = sc.nextInt();
+
+		if (num == 10) {
+			System.out.println("잘 따라 하는 ㅎ");
+		}
 	}
 
 }
