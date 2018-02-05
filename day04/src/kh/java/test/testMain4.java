03f7670ce04f851b85af961c530ecbcf9baf4a64
my Name <me@example.com>
2018-02-05 15:00:45 +0900

    -

--- day04/src/kh/java/test/testMain4.java
+++ day04/src/kh/java/test/testMain4.java
@@ -6,19 +6,19 @@ public class testMain4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int ans1, ans2;
+		int ans;
 		int count = 0;
 		// while (true) {
 		System.out.print("첫번째 퀴즈입니다.\n");
 		System.out.print("사과는 영어로 무엇일까요? (1. Apple / 2. 잡스): ");
-		ans1 = sc.nextInt();
+		ans = sc.nextInt();
 
-		if (ans1 == 1) {
+		if (ans == 1) {
 			System.out.println("정답!!");
 			count++;
 			System.out.print("두번째 퀴즈입니다.\n바나나는 길어 길으면 기차 기차는? (1. 빨라 / 2. 비싸): ");
-			ans2 = sc.nextInt();
-			if (ans2 == 1) {
+			ans = sc.nextInt();
+			if (ans == 1) {
 				System.out.println("정답!!");
 				count++;
 			} else {
@@ -26,11 +26,11 @@ public class testMain4 {
 			}
 			System.out.println("총 " + count + "문제를 맞추셨습니다\n");
 
-		} else if (ans1 == 2) {
+		} else if (ans == 2) {
 			System.out.println("땡!!");
 			System.out.print("두번째 퀴즈입니다.\n바나나는 길어 길으면 기차 기차는? (1. 빨라 / 2. 비싸): ");
-			ans2 = sc.nextInt();
-			if (ans2 == 1) {
+			ans = sc.nextInt();
+			if (ans == 1) {
 				System.out.println("정답!!");
 				count++;
 			} else {
---
+++ day04/src/kh/java/test/testMain5.java
@@ -0,0 +1,24 @@
+package kh.java.test;
+
+import java.util.Scanner;
+
+public class testMain5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("1부터 100까지의 수 중 선택: ");
+		int num1 = sc.nextInt();
+
+		if (num1 > 0 && num1 <= 100) {
+			if (num1 % 2 == 0) {
+				System.out.println("입력한 수는 짝수입니다.");
+			} else {
+				System.out.print("입력한 수는 홀수입니다.");
+			}
+		} else {
+			System.out.print("수를 잘못 입력했습니다.");
+		}
+
+	}
+
+}
