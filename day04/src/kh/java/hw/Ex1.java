6bd2f3dc6af72dbbc50eca1bb96bb9f149fd6d76
my Name <me@example.com>
2018-02-05 18:30:18 +0900

    -

---
+++ day04/src/kh/java/hw/Ex1.java
@@ -0,0 +1,12 @@
+package kh.java.hw;
+
+import java.util.Scanner;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("나이 입력 : ");
+		int age = sc.nextInt();
+		System.out.print((19<age)?"\"성인입니다. 어서오세요.\"":"\"미성년자는 입장불가입니다.\"");
+	}
+}
---
+++ day04/src/kh/java/test/testMain9.java
@@ -0,0 +1,25 @@
+package kh.java.test;
+
+import java.util.Scanner;
+
+public class testMain9 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("숫자를 입력하세요 : ");
+		int inNum = sc.nextInt();
+
+		if ((inNum % 3 == 0) && (inNum % 4 != 0)) {
+			System.out.println("[" + inNum + "]" + "은(는) 3의 배수입니다.");
+		} else if ((inNum % 3 == 0) && (inNum % 4 == 0) && inNum != 0) {
+			System.out.println("[" + inNum + "]" + "은(는) 3의 배수 이면서, 4의배수 입니다.");
+		} else if ((inNum % 3 != 0) && (inNum % 4 == 0)) {
+			System.out.println("[" + inNum + "]" + "은(는) 4의 배수입니다.");
+		} else if ((inNum % 3 != 0) && (inNum % 4 != 0)) {
+			System.out.println("[" + inNum + "]" + "은(는) 3의 배수도 4의 배수도 아닙니다.");
+		} else {
+			System.out.println("0 은(는) 3의 배수도 4의 배수도 아닙니다.");
+		}
+
+	}
+}
