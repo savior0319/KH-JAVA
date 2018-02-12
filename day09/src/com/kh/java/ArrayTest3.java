7b897ac87dab964fd3310592d3db2e874fbccfbe
savio <savio@192.168.219.104>
2018-02-13 00:51:36 +0900

    -

--- day09/src/com/kh/java/ArrayTest3.java
+++ day09/src/com/kh/java/ArrayTest3.java
@@ -2,87 +2,72 @@ package com.kh.java;
 
 public class ArrayTest3 {
 	public static void main(String[] args) {
-
 		int[][] arr = new int[5][5];
-
-		System.out.println("1번");
-		int k = 5;
+		int k = 1;
+		System.out.println();
+		System.out.println("1번.");
 		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				arr[i][j] = k--;
+			for (int j = 4; j >= 0; j--) {
+				arr[i][j] = k++;
 			}
-			k += 10;
 		}
 
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				System.out.printf("%5d", arr[i][j]);
+				System.out.printf("%5s", arr[i][j]);
+			}
+			System.out.println();
+		}		
+		System.out.println();
+		System.out.println("2번.");
+		k = 1;
+		for (int j = 0; j < 5; j++) {
+			for (int i = 0; i < 5; i++) {
+				arr[i][j] = k++;
+			}
+		}
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.printf("%5s", arr[i][j]);
 			}
 			System.out.println();
 		}
-
-		System.out.println("2번");
+		System.out.println();
+		System.out.println("3번.");
 		k = 1;
 		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
+			for (int j = 4; j >= 0; j--) {
 				arr[j][i] = k++;
 			}
 		}
-
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				System.out.printf("%5d", arr[i][j]);
+				System.out.printf("%5s", arr[i][j]);
 			}
 			System.out.println();
 		}
 
-		System.out.println("3번");
-		k = 5;
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				arr[i][j] = k;
-				k += 5;
-			}
-			k -= 26;
-		}
-
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				System.out.printf("%5d", arr[i][j]);
-			}
-			System.out.println();
-		}
-
-		System.out.println("3번");
+		System.out.println();
+		System.out.println("4번.");
 		k = 1;
 		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				arr[i][j] = k++;
+			if (i % 2 == 0) {
+				for (int j = 0; j < 5; j++) {
+					arr[i][j] = k++;
+				}
+			} else {
+				for (int j = 4; j >= 0; j--) {
+					arr[i][j] = k++;
+				}
 			}
 		}
 
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				System.out.printf("%5d", arr[i][j]);
+				System.out.printf("%5s", arr[i][j]);
 			}
 			System.out.println();
 		}
-		
-		
-		System.out.println("1번");
-		k = 1;
-		for (int i = 0; i < 5; i++) {
-			for (int j = 4; j < 0; j++) {
-				arr[i][j] = k++;
-			}
-		}
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				System.out.printf("%5d", arr[i][j]);
-			}
-			System.out.println();
-		}
-
-
 	}
 }
