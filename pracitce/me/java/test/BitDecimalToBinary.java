5dc5357ba17831a7e248eb8960effa86906490f5
savio <savio@192.168.219.104>
2018-02-25 23:13:25 +0900

    -

--- pracitce/me/java/test/BitDecimalToBinary.java
+++ pracitce/me/java/test/BitDecimalToBinary.java
@@ -3,21 +3,29 @@ package me.java.test;
 import java.util.Scanner;
 
 public class BitDecimalToBinary {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("-128 ~ 127 까지의 숫자 입력 : ");
-		Byte b = sc.nextByte();
-		String str = Integer.toBinaryString(b);
-
-		System.out.print("이진법으로 변환된 수 -> ");
-		if (b >= 0) {
-			for (int i = str.length(); i < 8; i++) {
-				System.out.print("0");
+		while (true) {
+			System.out.print("-128 ~ 127 까지의 숫자 입력 : ");
+			int temp = sc.nextInt();
+			if (-128 <= temp && temp < 127) {
+				Byte b = (byte) temp;
+				String str = Integer.toBinaryString(b);
+				System.out.print("변환된수 -> ");
+				if (b >= 0) {
+					for (int i = str.length(); i < 8; i++) {
+						System.out.print("0");
+					}
+					System.out.print(str);
+					break;
+				} else {
+					System.out.print(str.substring(24, 32));
+					break;
+				}
+			} else {
+				System.out.println("\n※ -128 ~ 127 까지의 숫자만 입력!!\n");
+				continue;
 			}
-			System.out.print(str);
-		} else {
-			System.out.print(str.substring(24, 32));
 		}
 	}
 }
