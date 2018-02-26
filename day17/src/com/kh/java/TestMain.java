caa6d37cfa3bf08576d45833ac6e992358df13b1
user1 <user1@user1-PC>
2018-02-26 12:17:23 +0900

    -

--- day17/src/com/kh/java/TestMain.java
+++ day17/src/com/kh/java/TestMain.java
@@ -1,10 +1,13 @@
 package com.kh.java;
 
+import java.util.Set;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		int a = 10;
 		double b = 0;
+		
 		System.out.println(Double.isFinite(a / b));
 	}
 }
