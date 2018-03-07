8817bb90e11dec5d3784bd4e55ca3286698b8f3b
savio <savio@192.168.219.104>
2018-03-07 20:43:18 +0900

    -

--- day23/src/kh/java/client/socket/TCPClientSocket.java
+++ day23/src/kh/java/client/socket/TCPClientSocket.java
@@ -8,25 +8,28 @@ import java.util.Scanner;
 
 public class TCPClientSocket {
 	public TCPClientSocket() {
-
 		Scanner sc = new Scanner(System.in);
 		final int PORT = 5606;
 		final String SERVER_IP = "localhost";
 		Socket csk = null;
 		try {
 			csk = new Socket(SERVER_IP, PORT);
-			System.out.println("서버와의 연결이 되었습니다");
+			System.out.println("※ 채팅 서버에 접속되었습니다!");
 			InputStream in = csk.getInputStream();
 			OutputStream out = csk.getOutputStream();
 			while (true) {
 				Date date = new Date();
-				SimpleDateFormat sdf = new SimpleDateFormat("[hh:mm:ss]");
-				String time = sdf.format(date);
+				SimpleDateFormat sdf = new SimpleDateFormat("[HH:mm:ss]");
+				String nowTime = sdf.format(date);
 				DataInputStream dis = new DataInputStream(in);
 				DataOutputStream dos = new DataOutputStream(out);
-				String data = dis.readUTF();
-				System.out.println(time + " 상대 -> " + data);
-				System.out.print(time + " 나 -> ");
+				String receive = dis.readUTF();
+				if(receive.equals("exit")) {
+					System.out.println("※ 종료합니다\n");
+					dis.close();
+				}
+				System.out.println(nowTime + " 상대 -> " + receive);
+				System.out.print(nowTime + " 나 -> ");
 				String send = sc.nextLine();
 				if(send.equals("exit")) {
 					System.out.println("※ 종료합니다\n");
--- day23/src/kh/java/server/run/ServerMain.java
+++ day23/src/kh/java/server/run/ServerMain.java
@@ -1,9 +1,9 @@
 package kh.java.server.run;
 
-import kh.java.server.socket.TCPServerSoket;
+import kh.java.server.socket.TCPServerSocket;
 
 public class ServerMain {
 	public static void main(String[] args) {
-		new TCPServerSoket();
+		new TCPServerSocket();
 	}
 }
